417e103667471d8fff3018da01ac0216a4edd2a8
Eutarides <javifarmacastillo@gmail.com>
2023-11-29 15:49:13 +0100

    tabs

--- src/tabs.js
+++ src/tabs.js
@@ -1,13 +1,22 @@
 export default (() => {
 
-    const imageMenu = document.querySelector('.image-menu');
-    const imageButtons = imageMenu.querySelectorAll('.image-menu-button');
+    // const imageMenu = document.querySelector('.image-menu');
+    // const imageButtons = imageMenu.querySelectorAll('.image-menu-button');
 
-    imageButtons.forEach(imageButton => {
-        imageButton.addEventListener('click', () => {
-            imageMenu.querySelector('.active').classList.remove('active');
-            imageButton.classList.add('active');
-        });
+    // imageButtons.forEach(imageButton => {
+    //     imageButton.addEventListener('click', () => {
+    //         imageMenu.querySelector('.active').classList.remove('active');
+    //         imageButton.classList.add('active');
+    //     });
+    // });
+
+    let imageMenu = document.querySelector('.image-menu');
+
+    imageMenu?.addEventListener('click', async (event) => {
+        if (event.target.closest('.image-menu-button')) {
+            let button = event.target.closest('.image-menu-button');
+            button.parentElement.querySelector('.active').classList.remove('active')
+            button.classList.add('active');
+        }
     });
-  
 })();
