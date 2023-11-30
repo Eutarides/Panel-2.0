a5a7b412d19b80369bb5ff7c2e5eed6cebb815e6
Eutarides <javifarmacastillo@gmail.com>
2023-11-30 09:31:56 +0100

    tabs

--- src/tabs.js
+++ src/tabs.js
@@ -11,12 +11,21 @@ export default (() => {
     // });
 
     let imageMenu = document.querySelector('.image-menu');
+    let formRows = document.querySelectorAll('.form-row');
 
     imageMenu?.addEventListener('click', async (event) => {
         if (event.target.closest('.image-menu-button')) {
             let button = event.target.closest('.image-menu-button');
             button.parentElement.querySelector('.active').classList.remove('active')
             button.classList.add('active');
+
+            formRows.forEach( formRow => {
+                if (button.dataset.value == formRow.dataset.value){
+                    formRow.classList.add('active');
+                }else if(button.dataset.value != formRow.dataset.value){
+                    formRow.classList.remove('active');
+                }
+            })
         }
     });
 })();
