6f45999bddc55acc37192fb42be2dc72e1efc586
Eutarides <javifarmacastillo@gmail.com>
2023-12-04 12:38:38 +0100

    creación rama web-components

--- src/hamburger.js
+++ src/hamburger.js
@@ -126,6 +126,7 @@ class Hamburger extends HTMLElement {
 
 
         hamburger.addEventListener("click",()=>{
+            
             curtain.classList.toggle("active");
             hamburger.classList.toggle("active");
         })
