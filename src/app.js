3ba036c30422fb835bc5ee4c5e23d2fc6c601766
Eutarides <javifarmacastillo@gmail.com>
2024-01-17 14:08:01 +0100

    estructura de carpetas

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import './filter-modal.js';
-import './validation.js';
-import './title.js';
-import './hamburger.js';
-import './table.js';
-import './delete-modal.js';
-import './client-form.js';
-import './notification.js';
